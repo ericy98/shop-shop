e2f6a89d0dfafe0c7ad07b256f4be3b1372b9881
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-22 11:17:51 -0500

    implement product & category idb storage

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -5,7 +5,8 @@ import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
-import spinner from "../../assets/spinner.gif"
+import { idbPromise } from '../../utils/helpers';
+import spinner from "../../assets/spinner.gif";
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
@@ -18,8 +19,19 @@ function ProductList() {
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+      
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) { // if loading is undefined, use indexedDB to get data
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
